1ac2162714cc3b364aa1ced3804c56bbcf79ec3e
Dawit Tekle <dvwish9214@gmail.com>
2021-07-09 21:58:33 +0300

    new branched created

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vercel demo done successfully
+          Vercel demo done successfully new branch
         </p>
         <a
           className="App-link"
